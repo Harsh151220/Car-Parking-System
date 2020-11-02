a75680cb68836bfc447a88d31a68981377cea1bf
Harsh151220 <72646964+Harsh151220@users.noreply.github.com>
2020-11-02 12:01:55 +0530

    Add files via upload

---
+++ Car Parking System/Car Parking System/src/Car.java
@@ -0,0 +1,33 @@
+
+public class Car extends Vehicle{
+	private int noOfDoors;
+	private String carColor;
+	private String carShade;
+	
+	public Car(String id, String brand, DateTime entryTime, int noOfDoors, String carColor, String carShade) {
+		super(id, brand, entryTime);
+		this.noOfDoors = noOfDoors;
+		this.carColor = carColor;
+		this.carShade = carShade;
+	}
+	public String getCarShade() {
+		return carShade;
+	}
+
+	public void setCarShade(String carShade) {
+		this.carShade = carShade;
+	}	
+
+	public int getNoOfDoors() {
+		return noOfDoors;
+	}
+	public void setNoOfDoors(int noOfDoors) {
+		this.noOfDoors = noOfDoors;
+	}
+	public String getCarColor() {
+		return carColor;
+	}
+	public void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+}
